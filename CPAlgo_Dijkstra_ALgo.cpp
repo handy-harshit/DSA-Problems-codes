d78abb2bc3cb1a6a1c75472cd849480ca2cca341
handy-harshit <harshitagarwal.23sept@gmail.com>
2021-04-13 12:10:48 +0530

    Initial commit

---
+++ CPAlgo_Dijkstra_ALgo.cpp
@@ -0,0 +1,39 @@
+while(t--){
+        int n,m,a,b,wt;
+        cin >> n >> m;
+        
+        while(m--){
+            cin>> a >> b >> wt;
+            adj[a].push_back({b,wt});
+            adj[b].push_back({a,wt});
+        }
+        int s;
+        cin >> s;
+        
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>> > minH;
+        vector<int> dist(n+1, INT_MAX);
+        vector<int> visited(n+1, 0);
+        
+        minH.push({0,s});
+        dist[s] = 0;
+        
+        while(!minH.empty()){
+            auto curr = minH.top();minH.pop();
+            int n1 = curr.second;
+            int n1_dist = curr.first;
+            
+            
+            //if(dist[n1] != n1_dist)continue;
+            if(visited[n1])continue;
+            visited[n1] = 1;
+            
+            for(auto edge : adj[n1]){
+                int n2 = edge.first;
+                int wt = edge.second;
+                if(dist[n1] + wt < dist[n2]){
+                    dist[n2] = dist[n1] + wt;
+                    minH.push({dist[n2],n2});
+                }
+                    
+            }
+        }
